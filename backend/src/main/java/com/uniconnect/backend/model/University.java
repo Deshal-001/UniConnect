014bfe212adf9ff457221d3cc7280b28453a5faa
Kavindu Deshal <85722430+Deshal-001@users.noreply.github.com>
2025-04-19 18:51:05 +0200

    Implemented Join Events Feature and Connected to PostgresSQL DB

---
+++ backend/src/main/java/com/uniconnect/backend/model/University.java
@@ -0,0 +1,23 @@
+package com.uniconnect.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "university")
+public class University {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+
+    private String location;
+
+}
