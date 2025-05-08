28d5bf3f219f440650daf18083460dc2d48526c4
Kavindu Deshal <85722430+Deshal-001@users.noreply.github.com>
2025-05-08 10:56:58 +0200

    updated

--- mobile/mobile_flutter/lib/feature/authentication/domain/repositories/auth_repository_impl.dart
+++ mobile/mobile_flutter/lib/feature/authentication/domain/repositories/auth_repository_impl.dart
@@ -11,39 +11,36 @@ import '../../data/repositories/auth_repository.dart';
 class AuthRepoImplementation implements AuthenticationRepository {
   final AuthApiRemoteDataSource api;
 
-  AuthRepoImplementation(
-    this.api,
-  );
+  AuthRepoImplementation(this.api);
 
   @override
   Future<Either<Failure, String>> login(String email, String password) async {
-    Logger().i('Login attempt with email: $email and password: $password');
     try {
       final params = {
         'email': email,
         'password': password,
       };
       final response = await api.login(params);
+
       if (response.token.isEmpty) {
-        throw const ApiException(statusCode: '401',message:  'Invalid credentials');
+        throw const ApiException(
+            statusCode: '401', message: 'Invalid credentials');
       }
-      // Store the JWT token securely
-      TokenController.storeTokens({
+      // Store the JWT token
+      await TokenController.storeTokens({
         TokenConstants.jwt: response.token,
       });
+
       return Right(response.token);
     } on DioException catch (e) {
-     
-      return Left(
-        ApiException
-        .fromDioException(e)
-      );
+      return Left(ApiException.fromDioException(e));
     } catch (e) {
-      return 
-      Left(ApiException(message: '500', statusCode: 'Unexpected error: ${e.toString()}'));
+      return const Left(ApiException(
+          message: 'Unexpected error occurred', statusCode: '500'));
     }
   }
 
+  // Uncomment and implement the register method if needed
   // @override
   // Future<String> register({required String email, required String password, required String fullName}) async {
   //   final response = await api.register({
@@ -51,7 +48,9 @@ class AuthRepoImplementation implements AuthenticationRepository {
   //     'password': password,
   //     'fullName': fullName,
   //   });
-  //   await storage.write(key: 'jwt', value: response.token);
+  //   await TokenController.storeTokens({
+  //     TokenConstants.jwt: response.token,
+  //   });
   //   return response.token;
   // }
 }
