9c73054f6eba8cbe2c64539880683b53079f3889
Kavindu Deshal <85722430+Deshal-001@users.noreply.github.com>
2025-05-08 15:36:29 +0200

    Updated the custom text field

--- mobile/mobile_flutter/lib/main.dart
+++ mobile/mobile_flutter/lib/main.dart
@@ -3,8 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:uniconnect_app/core/router/app_router.dart';
 import 'package:uniconnect_app/feature/authentication/presentation/bloc/authentication_bloc.dart';
 
-import 'core/injection_container.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'injection_container.dart';
 
 
 void main() async {
